fe090ca9443068213e1d1a6ebb53d8a6598d0b36
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-08-17 00:34:20 -0300

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,9 @@ let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-body: '⫷᭄ʜᴀᴅᴇs-ʙᴏᴛ-ᴍᴅ﹏✍',         
+body: '▒̶᩠߲⃡ࠖ᩼᭭݊⃝⃡🩵ࣱꪾ῾֣ؐ  𝘈𝘯𝘯𝘪𝘦𝘉𝘰𝘵 °୭',         
 previewType: 0, thumbnail: fs.readFileSync("./views/Menu.jpg"),
-sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD.git`}}})   
+sourceUrl: `https://github.com/4nni33/Annie.git`}}})   
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
