303ba1222b09115ce343cf51926198f01b955181
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-08-17 15:12:26 -0300

    Update Anti-links.js

--- plugins/Anti-links.js
+++ plugins/Anti-links.js
@@ -17,13 +17,13 @@ const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.
 if (m.text.includes(linkThisGroup)) return !0
 }    
 await this.sendMessage(m.chat, { text: `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*¡Hasta nunca! 👋 ${user} Rompiste las reglas del grupo, serás purgado*`, mentions: [m.sender] }) 
-if (!isBotAdmin) return m.reply('*[❗ERROR❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*')  
+if (!isBotAdmin) return m.reply('*[❗ERROR❗] El bot no es admin, no puede iniciar la purga*')  
 //await conn.sendButton(m.chat, `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*¡Hasta nunca! 👋, ${await this.getName(m.sender)} Rompiste las reglas del grupo, serás purgado*${isBotAdmin ? '' : '\n\n*[❗ERROR❗] El bot no es admin, no puede iniciar la purga*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
-} else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] El propietario del bot no tiene habilitadas las restricciones, (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) contactelo para que las habilite.*')
+} else if (!bot.restrict) return m.reply('*[❗ERROR❗] El propietario del bot no tiene habilitadas las restricciones, (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) contactelo para que las habilite.*')
 }
 return !0
 }
