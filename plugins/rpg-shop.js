91a6bdbb1b6b5627f0f76bf2ce09b33a6bcfe55e
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-17 17:56:07 -0300

    Update rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,23 +1,32 @@
-const xpperlimit = 50
+const xpperlimit = 50;
+
 let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^buy/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
-  count = Math.max(1, count)
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
-    global.db.data.users[m.sender].exp -= xpperlimit * count
-    global.db.data.users[m.sender].limit += count
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
+
+    // Guardar los cambios en la base de datos (asegúrate de que tu sistema de base de datos esté configurado correctamente)
+    // global.db.save();
+
     conn.reply(m.chat, `╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
 │🛒 *NOTA DE PAGO* 
 │┈┈┈┈┈┈┈┈┈┈┈┈┈
-│➯‣ *Compra nominal* : + ${count}💎 
-│➯‣ *Gastado* : -${xpperlimit * count} XP
-╰───────────────╯`, m)
-  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m)
-}
-handler.help = ['Buy', 'Buyall']
-handler.tags = ['xp']
-handler.command = ['buy', 'buyall'] 
-handler.register = true
-handler.disabled = false
+│➯‣ *Compra nominal*: +${count}💎 
+│➯‣ *Gastado*: -${xpperlimit * count} XP
+╰───────────────╯`, m);
+  } else {
+    conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m);
+  }
+};
 
-export default handler
+handler.help = ['Buy', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['buy', 'buyall'];
+handler.register = true;
+handler.disabled = false;
+
+export default handler;
