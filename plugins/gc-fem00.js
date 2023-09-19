146545d4407f85e5dd4e0396e66155f86de56ebe
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-18 23:21:57 -0300

    Update gc-fem00.js

--- plugins/gc-fem00.js
+++ plugins/gc-fem00.js
@@ -1,40 +1,42 @@
+let nombreEncargada = "Encargada"; // Nombre predeterminado
+let textoPersonalizable = "Texto personalizable"; // Texto predeterminado
+
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `THE ZERO LISTA FEM💞: ${pesan}`
-let teks = `╭┈┈ ๑❀๑ •• ${oi} ๑❀๑ ••:\n`
-for (let mem of participants) {
-teks += `@${mem.id.split('@')[0]}🍃`}
-teks += `ㅤ
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn)
+    throw false
+  }
+
+  let nuevoNombre = args[0] || nombreEncargada; // Obtiene el nombre nuevo o usa el predeterminado
+  let nuevoTexto = args.slice(1).join(' ') || textoPersonalizable; // Obtiene el texto nuevo o usa el predeterminado
+  
+  let oi = `𝗟𝗶𝘀𝘁𝗮 𝗱𝗲 𝗩𝗦`;
+  let teks = `╭┈┈ ৎ 🪷ִ ׂ ⬫ ${oi}  ָ࣪  ۰ . ¡! \n`
+  teks += `│
+│      ෆ *𝘌𝘯𝘤𝘢𝘳𝘨𝘢𝘥𝘢:*  ${nuevoNombre}
+│      ෆ *𝘏𝘰𝘳𝘢𝘳𝘪𐐫:*
+│         ‧₊⌗ 22 🇵🇪 ⌇ 23 🇨🇱 ⌇ 00 🇦🇷      
 │
-│         💗 ZERO, BOT SIMPLE CREADOR:http://wa.me/59897463355 💗
-│        ₊˚︶︶︶︶︶︶︶︶˚ ‧₊
-│
-│      ෆ 𝘌𝘯𝘤𝘢𝘳𝘨𝘢𝘥𝘢:
-│      ෆ 𝘏𝘰𝘳𝘢𝘳𝘪𐐫:
-│         ‧₊⌗ 00 🇵🇪 ⌇ 01 🇨🇱 ⌇ 02 🇦🇷      
-│
-│ㅤㅤʚ 𝘑𝘶𝘨𝘢𝘥𝘰𝘳𝘢𝘴:
-│ㅤ🔞• 
-│ㅤ🔞• 
-│ㅤ🔞• 
-│    🔞•
+│ㅤㅤʚ *𝘑𝘶𝘨𝘢𝘥𝘰𝘳𝘢𝘴:*
+│ㅤ𐀔 • 
+│ㅤ𐀔 • 
+│ㅤ𐀔 • 
+│ㅤ𐀔 • 
 │   ㅤㅤ
-│ㅤㅤʚ 𝘚𝘶𝘱𝘭𝘦𝘯𝘵𝘦𝘴:
-│ㅤ🔞•
-│ㅤ🔞•
+│ㅤㅤʚ *𝘚𝘶𝘱𝘭𝘦𝘯𝘵𝘦𝘴:*
+│ㅤᵎᵎ •
+│ㅤᵎᵎ •
 │
-│   ⊹ ִֶָ  𝘋𝘰𝘯𝘢𝘥𝘰𝘳𝘢 𝘥𝘦 𝘴𝘢𝘭𝘢: 
-│
+│   ⊹ ִֶָ  𝘋𝘰𝘯𝘢𝘥𝘰𝘳𝘢 𝘥𝘦 𝘴𝘢𝘭𝘢:
 ╰───────────────๑❀๑ •`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+
+  conn.sendMessage(m.chat, { text: teks }, )
 }
-handler.help = ['smsf00 <mesaje>','fem00 <mesaje>']
+
+handler.help = ['smsf00 <mensaje>', 'fem00 <mensaje>']
 handler.tags = ['group']
 handler.command = /^(smsf00|fem00)$/i
 handler.admin = true
 handler.group = true
+
 export default handler
