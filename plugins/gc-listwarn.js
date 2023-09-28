c513629792df6c679d1136fc2c42f8b9f07103eb
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-28 22:21:57 +0200

    Update gc-listwarn.js

--- plugins/gc-listwarn.js
+++ plugins/gc-listwarn.js
@@ -4,11 +4,11 @@ let warns = global.db.data.users.warn
 let user = global.db.data.users
 let imagewarn = './src/warn.jpg'
 let caption = `⚠️ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝙳𝙾𝚂\n 
-*╔═══════════════════·•*
-║ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
-║
-║ 1.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/3)*\n║\n║ - - - - - - - - -`.trim()).join('\n') : ''}
-*╚═══════════════════·•*`
+╭───────────────๑🫐๑
+│ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
+│
+│ 1.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/3)*\n│\n│ - - - - - - - - -`.trim()).join('\n') : ''}
+╰───────────────๑✨๑`
 await conn.sendButton(m.chat, caption, wm, imagewarn, [['𝐌 𝐄 𝐍 𝐔 🌠', '#menu']], m, {mentions: await conn.parseMention(caption)})}
 handler.command = /^(listwarn)$/i 
 handler.group = true
