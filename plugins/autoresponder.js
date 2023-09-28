a0cf230725621606030b1287e080f8f097c77216
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-28 22:09:55 +0200

    Update autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '▒̶᩠߲⃡ࠖ᩼᭭݊⃝⃡🩵ࣱꪾ῾֣ؐ  𝘈𝘯𝘯𝘪𝘦𝘉𝘰𝘵 °୭', body: '@designs.annie', sourceUrl: `https://github.com/4nni33/Annie`, thumbnail: Menu}}})}
+contextInfo: { externalAdReply: { title: '▒̶᩠߲⃡ࠖ᩼᭭݊⃝⃡🫐ࣱꪾ῾֣ؐ  𝘟𝘪𝘱𝘢𝘉𝘰𝘵 °୭', body: '@xipa_dzn', sourceUrl: `https://github.com/4nni33/XipaBot`, thumbnail: Menu}}})}
     
 return !0 }
 handler.register = true
